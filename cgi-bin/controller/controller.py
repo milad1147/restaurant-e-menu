18fe3890c4e2b42fbe2f3946a79d2c5ce1b6a647
gantonov <g_antonov@gsvision.eu>
2014-06-05 19:02:08 +0300

    Exception converted to string so that it can be json encoded

--- cgi-bin/controller/controller.py
+++ cgi-bin/controller/controller.py
@@ -3,20 +3,17 @@ class Controller:
         pass  # TODO
 
     def call(self, methodName, *params):
-        try:
-            if self.checkPermissions(methodName):
-                result = getattr(self, methodName)(*params)
-                return {
-                    'status': True,
-                    'data': result
-                }
-            else:
-                return {
-                    'status': False,
-                    'message': 'access denied'
-                }
-        except Exception as e:
-            return {'status': False, 'message': str(e)}
+        if self.checkPermissions(methodName):
+            result = getattr(self, methodName)(*params)
+            return {
+                'status': True,
+                'data': result
+            }
+        else:
+            return {
+                'status': False,
+                'message': 'access denied'
+            }
 
     def checkPermissions(self, methodName):
         return True
